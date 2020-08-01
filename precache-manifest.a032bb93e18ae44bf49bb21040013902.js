04ef7d94fb1f9898a9ca60e7ac036323c93f8c34
KEVIN NAYLOR <kevin@KLNMBP13.fios-router.home>
2020-08-01 09:31:00 -0400

    Updates

---
+++ precache-manifest.a032bb93e18ae44bf49bb21040013902.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92170ce4ea1be810ad873d32bc377ff",
+    "url": "/johnlagio/index.html"
+  },
+  {
+    "revision": "87fe40858489de40cd56",
+    "url": "/johnlagio/static/css/main.9b78c49a.chunk.css"
+  },
+  {
+    "revision": "13a14521206d09e46197",
+    "url": "/johnlagio/static/js/2.0fdfc70c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/johnlagio/static/js/2.0fdfc70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87fe40858489de40cd56",
+    "url": "/johnlagio/static/js/main.435ea98a.chunk.js"
+  },
+  {
+    "revision": "8c570a2faa59b470a50f",
+    "url": "/johnlagio/static/js/runtime-main.130881fb.js"
+  },
+  {
+    "revision": "57628f430e7acd049ca0ccc5ce03f317",
+    "url": "/johnlagio/static/media/decorations_01.57628f43.jpeg"
+  },
+  {
+    "revision": "539ee7b8331f62ab52e58450fbf9f3a2",
+    "url": "/johnlagio/static/media/firepit-dusk.539ee7b8.jpeg"
+  },
+  {
+    "revision": "651a0eea7232dacc81d286f15bc6f74e",
+    "url": "/johnlagio/static/media/food_01.651a0eea.jpeg"
+  },
+  {
+    "revision": "7e7fe7a2070e4d705b3b713890e4fe83",
+    "url": "/johnlagio/static/media/garden.7e7fe7a2.jpeg"
+  },
+  {
+    "revision": "2f1f986820a99197250b55fa75c1e78e",
+    "url": "/johnlagio/static/media/grill.2f1f9868.jpeg"
+  },
+  {
+    "revision": "bf321bc78bd518e706ff82cafcdd3aee",
+    "url": "/johnlagio/static/media/hammock.bf321bc7.jpeg"
+  },
+  {
+    "revision": "52c1ce1499a0af96ef34d1576e42d96f",
+    "url": "/johnlagio/static/media/johnlagio.52c1ce14.jpeg"
+  },
+  {
+    "revision": "52bb5c1e0fa9ae41537a6bed1359451f",
+    "url": "/johnlagio/static/media/management.52bb5c1e.jpeg"
+  },
+  {
+    "revision": "bb92d4a59fafee07188c090650b04cc7",
+    "url": "/johnlagio/static/media/pool_01.bb92d4a5.jpeg"
+  }
+]);
